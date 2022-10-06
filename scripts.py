b409df0512fc49e4106ba5efa84e912f492cf037
paulego <egopaul@inbox.ru>
2022-10-06 13:00:03 +0500

    Added remove_chastisements function.

--- scripts.py
+++ scripts.py
@@ -1,8 +1,16 @@
-from datacenter.models import (Schoolkid, Mark)
+from datacenter.models import (Schoolkid, Mark, Chastisement)
+
+childs_name, childs_id = ['Фролов Иван', 'Голубев Феофан'], [6551, 6548]
+
+child = Schoolkid.objects.filter(full_name__contains=childs_name[0])
+bad_marks = Mark.objects.filter(schoolkid=child[0], points__lte=3)
 
 
-child = Schoolkid.objects.get(pk=6551)
-bad_marks = Mark.objects.filter(schoolkid=child, points__lte=3)
+def remove_chastisements(schoolkid: Schoolkid):
+    notes = Chastisement.objects.filter(schoolkid_id=schoolkid.pk)
+    for note in notes:
+        note.delete()
+        print('Замечания удалены.')
 
 
 def fix_marks(schoolkid: Schoolkid):
@@ -10,7 +18,9 @@ def fix_marks(schoolkid: Schoolkid):
     for point in bad_points:
         point.points = 5
         point.save()
+    print('Оценки исправлены.')
 
 
-fix_marks(child)
+fix_marks(child[0])
+remove_chastisements(child[0])
 bad_marks.count()
